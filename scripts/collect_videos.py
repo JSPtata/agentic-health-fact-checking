ff618f560f22dc8c8d49dee3852c52d3b523ce31
Pranathi <tatajspranathi@gmail.com>
2026-09-13 23:52:31 +0530

    Add video collection and start audio preprocessing

--- scripts/collect_videos.py
+++ scripts/collect_videos.py
@@ -3,21 +3,38 @@ import yt_dlp #type:ignore
 
 ydl_opts={}
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    with open("data/manifest_v2.csv") as file:
-        reader=csv.DictReader(file)
-
+def csv_to_list(filename):
+    result_list=[]
+    with open(filename) as file_obj:
+        reader=csv.DictReader(file_obj,delimiter=",")
         for row in reader:
-            if row["status"]=="pending":
-                video_id=row["video_id"]
-                url=row["url"]
+            result_list.append(row)
+    
+    return result_list
 
-                output=f"data/raw/videos/{video_id}.%(ext)s"
+rows=csv_to_list("data/manifest_v2.csv")
 
-                ydl_opts={
-                    'outtmpl':output
-                }
-                print("Downloading:",row["video_id"])
-                ydl.download([url])
-                break
 
+for row in rows:
+    if(row["status"]=="pending"):
+        url=row["url"]
+        video_id=row["video_id"]
+
+        ydl_opts={
+            "outtmpl":f"data/raw/videos/{video_id}.%(ext)s"
+        }
+
+        try:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.extract_info(url,download=True)
+            row["status"]="downloaded"
+        except yt_dlp.utils.DownloadError as error:
+            row["status"]="download_error"
+            row["notes"]=str(error)
+
+
+        
+with open("data/manifest_v2.csv","w",newline="") as file:
+    writer=csv.DictWriter(file,fieldnames=["video_id","category","url","status","notes"])
+    writer.writeheader() 
+    writer.writerows(rows) 
