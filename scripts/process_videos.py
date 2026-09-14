e73510310c5a491d06f93a7e5e642c7df4170262
Pranathi <tatajspranathi@gmail.com>
2026-09-14 20:55:36 +0530

    Add audio extraction and Whisper transcription

--- scripts/process_videos.py
+++ scripts/process_videos.py
@@ -1,10 +1,11 @@
 import subprocess
 import os
 import sys
+import whisper #type:ignore
 
 def extract_audio(video_file,output_ext="wav"):
     base=os.path.basename(video_file)
-    name,ext=os.path.splitext(video_file)
+    name,ext=os.path.splitext(base)
 
     audio_name=name+".wav"
     output_path=os.path.join("data/processed/audio",audio_name)
@@ -12,12 +13,25 @@ def extract_audio(video_file,output_ext="wav"):
         ["ffmpeg",
          "-y",
          "-i",
-         "video_file",
-         f"{name}.{output_ext}"],
+         video_file,
+         f"{output_path}"],
          stdout=subprocess.DEVNULL,
          stderr=subprocess.STDOUT
     )
 
+    return output_path
+
+
+def transcribe_audio(audio_file):
+    model=whisper.load_model("small")
+    result=model.transcribe(audio_file)
+    print(result["text"])
+    print(result.keys())
+    print(result["segments"])
+    return result
+
+
 if __name__ == "__main__":
     vf = sys.argv[1]
-    extract_audio(vf)
+    audio_path=extract_audio(vf)
+    result=transcribe_audio(audio_path)
